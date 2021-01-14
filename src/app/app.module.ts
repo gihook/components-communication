44a22d13b718391aa29eed56992a5377c89c83b8
Nikola Djukanovic <nikola.djukanovic@rationaletech.com>
2021-01-14 21:48:25 +0100

    feat: created basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { ContentContainerComponent } from './components/content-container/content-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    SideMenuComponent,
+    ContentContainerComponent
   ],
   imports: [
     BrowserModule
