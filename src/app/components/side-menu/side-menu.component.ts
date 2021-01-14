fc6f19171ad2d0eaa505e9e33c9337da00c0ca00
Nikola Djukanovic <nikola.djukanovic@rationaletech.com>
2021-01-14 22:29:56 +0100

    feat: added selected class

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  id: number;
+  displayValue: string;
+}
+
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.scss'],
 })
 export class SideMenuComponent implements OnInit {
-  menuItems: { id: number; displayValue: string }[] = [];
-
-  constructor() {}
+  selectedItemId: number;
+  menuItems: MenuItem[] = [];
 
   ngOnInit(): void {
     this.menuItems = [
