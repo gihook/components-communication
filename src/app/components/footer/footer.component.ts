0ab9a0f48e387da82411fc2d6443c6c6ef4a07c4
Nikola Djukanovic <nikola.djukanovic@rationaletech.com>
2021-01-14 23:10:15 +0100

    feat: implemented item state

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { CommunicationService } from 'src/app/services/communication.service';
+import { ComputerItem } from '../models/models';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  computerItem: ComputerItem;
+  constructor(private communicationService: CommunicationService) {}
 
   ngOnInit(): void {
+    this.communicationService.registerAction((ci) => (this.computerItem = ci));
   }
-
 }
--- src/app/services/communication.service.ts
+++ src/app/services/communication.service.ts
@@ -5,13 +5,19 @@ import { ComputerItem } from '../components/models/models';
   providedIn: 'root',
 })
 export class CommunicationService {
+  computerItem: ComputerItem;
   actions: ((ci: ComputerItem) => void)[] = [];
 
   registerAction(action: (ci: ComputerItem) => void) {
     this.actions = [action, ...this.actions];
+
+    if (!this.computerItem) return;
+
+    this.emitItem(this.computerItem);
   }
 
   emitItem(item: ComputerItem) {
+    this.computerItem = item;
     this.actions.forEach((action) => action(item));
   }
 }
