3a4ddff7b3b2a496470abcdf8f49715388759eaf
Nikola Djukanovic <nikola.djukanovic@rationaletech.com>
2021-01-14 22:37:56 +0100

    feat: binding to single component

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -3,6 +3,12 @@ import { Component, OnInit } from '@angular/core';
 interface MenuItem {
   id: number;
   displayValue: string;
+  isSelected: boolean;
+}
+
+interface ComputerItem {
+  id: number;
+  displayValue: string;
 }
 
 @Component({
@@ -12,14 +18,36 @@ interface MenuItem {
 })
 export class SideMenuComponent implements OnInit {
   selectedItemId: number;
+  computerItems: ComputerItem[] = [];
   menuItems: MenuItem[] = [];
 
   ngOnInit(): void {
-    this.menuItems = [
+    const computerItems = [
       { id: 1, displayValue: 'Monitor' },
       { id: 2, displayValue: 'Keyboard' },
       { id: 3, displayValue: 'Laptop' },
       { id: 4, displayValue: 'Mouse' },
     ];
+
+    this.bindMenuItems(computerItems);
+  }
+
+  private bindMenuItems(computerItems: ComputerItem[]) {
+    this.menuItems = computerItems.map((computerItem) => {
+      const isSelected = this.isSelectedItem(computerItem.id);
+      const menuItem = { ...computerItem, isSelected };
+
+      return menuItem;
+    });
+  }
+
+  isSelectedItem(itemId: number) {
+    console.log({ isSelectedItem: true });
+    return this.selectedItemId === itemId;
+  }
+
+  selectItem(item: MenuItem) {
+    this.selectedItemId = item.id;
+    this.bindMenuItems(this.menuItems);
   }
 }
