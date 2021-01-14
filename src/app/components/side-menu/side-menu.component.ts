af5bf70ed77d3c7325edd210fb8fa878a6ad4b6a
Nikola Djukanovic <nikola.djukanovic@rationaletech.com>
2021-01-14 22:14:01 +0100

    feat: finished initialization

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.scss']
+  styleUrls: ['./side-menu.component.scss'],
 })
 export class SideMenuComponent implements OnInit {
+  menuItems: { id: number; displayValue: string }[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.menuItems = [
+      { id: 1, displayValue: 'Monitor' },
+      { id: 2, displayValue: 'Keyboard' },
+      { id: 3, displayValue: 'Laptop' },
+      { id: 4, displayValue: 'Mouse' },
+    ];
   }
-
 }
